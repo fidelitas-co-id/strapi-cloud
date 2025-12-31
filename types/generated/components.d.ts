00ce2be0526f54a841bf0273e061cfdca181cce9
fidelitas-co-id <admin@fidelitas.co.id>
2026-01-01 03:36:14 +0700

    Add SEO plugin, update dependencies, and restructure content types

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,75 +1,213 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface SharedSlider extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sliders';
+export interface BlocksCards extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_cards';
   info: {
-    displayName: 'Slider';
-    icon: 'address-book';
-    description: '';
+    displayName: 'Cards';
   };
   attributes: {
-    files: Schema.Attribute.Media<'images', true>;
+    cards: Schema.Attribute.Component<'elements.card', true>;
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksConsultantSpotlight extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_consultant_spotlights';
+  info: {
+    displayName: 'consultantSpotlight';
+  };
+  attributes: {
+    consultant: Schema.Attribute.Relation<
+      'oneToOne',
+      'api::consultant.consultant'
+    >;
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksCta extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_ctas';
+  info: {
+    displayName: 'Cta';
+  };
+  attributes: {
+    cards: Schema.Attribute.Component<'blocks.cards', true>;
+  };
+}
+
+export interface BlocksHeroWithBgImage extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_hero_with_bg_images';
+  info: {
+    displayName: 'heroWithBgImage';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    lists: Schema.Attribute.Component<'elements.list', true>;
+    subHeading: Schema.Attribute.Text;
+  };
+}
+
+export interface BlocksHeroWithBgVideo extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_hero_with_bg_videos';
+  info: {
+    displayName: 'heroWithBgVideo';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    link: Schema.Attribute.Component<'elements.link', false>;
+    video: Schema.Attribute.Media<'videos'>;
+  };
+}
+
+export interface BlocksRelatedPosts extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_related_posts';
+  info: {
+    displayName: 'relatedPosts';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    link: Schema.Attribute.Component<'elements.link', false>;
+  };
+}
+
+export interface BlocksServiceOfferings extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_service_offerings';
+  info: {
+    displayName: 'serviceOfferings';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    link: Schema.Attribute.Component<'elements.link', false>;
+  };
+}
+
+export interface BlocksTextWithSideImage extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_text_with_side_images';
+  info: {
+    displayName: 'textWithSideImage';
+  };
+  attributes: {
+    description: Schema.Attribute.RichText;
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    imagePosition: Schema.Attribute.Enumeration<['left', 'right']>;
+    json: Schema.Attribute.JSON;
+  };
+}
+
+export interface ElementsCard extends Struct.ComponentSchema {
+  collectionName: 'components_elements_cards';
+  info: {
+    displayName: 'Card';
+  };
+  attributes: {
+    description: Schema.Attribute.String;
+    icon: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    link: Schema.Attribute.Component<'elements.link', false>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface ElementsLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    IsExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface ElementsList extends Struct.ComponentSchema {
+  collectionName: 'components_elements_lists';
+  info: {
+    displayName: 'List';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedOpenGraph extends Struct.ComponentSchema {
+  collectionName: 'components_shared_open_graphs';
+  info: {
+    displayName: 'openGraph';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    ogDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 160;
+      }>;
+    ogImage: Schema.Attribute.Media<'images'>;
+    ogTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 70;
+      }>;
+    ogType: Schema.Attribute.String;
+    ogUrl: Schema.Attribute.String;
   };
 }
 
 export interface SharedSeo extends Struct.ComponentSchema {
   collectionName: 'components_shared_seos';
   info: {
-    name: 'Seo';
-    icon: 'allergies';
+    description: '';
     displayName: 'Seo';
-    description: '';
+    icon: 'allergies';
+    name: 'Seo';
   };
   attributes: {
-    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
-    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
-    shareImage: Schema.Attribute.Media<'images'>;
-  };
-}
-
-export interface SharedRichText extends Struct.ComponentSchema {
-  collectionName: 'components_shared_rich_texts';
-  info: {
-    displayName: 'Rich text';
-    icon: 'align-justify';
-    description: '';
-  };
-  attributes: {
-    body: Schema.Attribute.RichText;
-  };
-}
-
-export interface SharedQuote extends Struct.ComponentSchema {
-  collectionName: 'components_shared_quotes';
-  info: {
-    displayName: 'Quote';
-    icon: 'indent';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    body: Schema.Attribute.Text;
-  };
-}
-
-export interface SharedMedia extends Struct.ComponentSchema {
-  collectionName: 'components_shared_media';
-  info: {
-    displayName: 'Media';
-    icon: 'file-video';
-  };
-  attributes: {
-    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    canonicalURL: Schema.Attribute.String;
+    keywords: Schema.Attribute.Text;
+    metaDescription: Schema.Attribute.Text &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 160;
+        minLength: 50;
+      }>;
+    metaImage: Schema.Attribute.Media<'images'>;
+    metaRobots: Schema.Attribute.String &
+      Schema.Attribute.DefaultTo<'follow, index'>;
+    metaTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 70;
+      }>;
+    metaViewport: Schema.Attribute.String;
+    openGraph: Schema.Attribute.Component<'shared.open-graph', false>;
+    structuredData: Schema.Attribute.JSON;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'shared.slider': SharedSlider;
+      'blocks.cards': BlocksCards;
+      'blocks.consultant-spotlight': BlocksConsultantSpotlight;
+      'blocks.cta': BlocksCta;
+      'blocks.hero-with-bg-image': BlocksHeroWithBgImage;
+      'blocks.hero-with-bg-video': BlocksHeroWithBgVideo;
+      'blocks.related-posts': BlocksRelatedPosts;
+      'blocks.service-offerings': BlocksServiceOfferings;
+      'blocks.text-with-side-image': BlocksTextWithSideImage;
+      'elements.card': ElementsCard;
+      'elements.link': ElementsLink;
+      'elements.list': ElementsList;
+      'shared.open-graph': SharedOpenGraph;
       'shared.seo': SharedSeo;
-      'shared.rich-text': SharedRichText;
-      'shared.quote': SharedQuote;
-      'shared.media': SharedMedia;
     }
   }
 }
